5dd8eb6780ac7721621fdb7d1426fea854c0db3e
Fanny Rostaing <fanny@coffretis.com>
2020-08-26 12:40:04 +0200

    v1 ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,12 @@
 class ProductsController < ApplicationController
 
-
   def show
-    barcode = "3263850365416"
+    barcode = "3564700661420"
     @product = Product.from_barcode(barcode)
 
     redirect_to new_product_component_path(@product) unless @product.clean?
   end
 
   def scan
-
   end
-
 end
