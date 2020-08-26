b3ebd137ea2fd894ed3b5b8f36b1ab4755784a2f
Hugo Fortunier <hfortunier@gmail.com>
2020-08-26 09:10:04 +0200

    maj du matin

--- app/javascript/scanner/scanner.js
+++ app/javascript/scanner/scanner.js
@@ -15,23 +15,25 @@ const quaggaScanner = () => {
     decoder : {
       readers : ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader", "2of5_reader","code_93_reader"],
       debug: {
-      drawBoundingBox: true,
-      drawScanline: true,
-      showPattern: true
+        drawBoundingBox: true,
+        drawScanline: true,
+        showPattern: true
+      }
     }
-  }, function(err) {
+  },function(err) {
       if (err) {
-          console.log(err);
-          return
+        console.log(err);
+        return
       }
       console.log("Initialization finished. Ready to start");
       Quagga.start();
-  }});
-
-Quagga.onDetected(function(result) {
- const barcode = result.codeResult.code;
- Quagga.stop();
-});
+      Quagga.onDetected(function(result) {
+        const barcode = result.codeResult.code;
+        console.log(barcode);
+        Quagga.stop();
+      })
+    }
+  );
 };
 
 export { quaggaScanner }
