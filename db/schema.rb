8644b66e430f9a3ffd534bcef5793bb70c4aeca3
Noor Makria noor.makria@gmail.com <noor.makria@gmail.com>
2020-09-02 16:12:09 +0200

    carte home page content done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_092705) do
+ActiveRecord::Schema.define(version: 2020_09_02_135249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 2020_09_02_092705) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo_url"
-    t.jsonb "instruction"
-    t.text "contact"
+    t.string "instruction", array: true
+    t.string "contact", array: true
   end
 
   create_table "products", force: :cascade do |t|
