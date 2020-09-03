f4e1ecad3ac3000e67ade237cab7fdeed92233fc
Noor Makria noor.makria@gmail.com <noor.makria@gmail.com>
2020-09-03 12:28:25 +0200

    meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def check_location
     redirect_to '/we_need_your_position_please' unless cookies[:city]
   end
+
+  def default_url_options
+    { host: ENV["wwww.the-good-wayste.co"] || "localhost:3000" }
+  end
 end
