f9214152eb719ff87e186df5919593b02201441a
Hugo Fortunier <hfortunier@gmail.com>
2020-09-04 13:54:57 +0200

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @place = Place.find_by_name_without_case(cookies[:city])
+
+    if @place.blank?
+      @place = Place.last
+    end
   end
 
   def position
