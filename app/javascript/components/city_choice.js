145310620a9f07648bebb39f600090211c513f00
Hugo Fortunier <hfortunier@gmail.com>
2020-09-04 17:32:01 +0200

    Fix form submission bug

--- app/javascript/components/city_choice.js
+++ app/javascript/components/city_choice.js
@@ -1,15 +1,16 @@
 const newLocation = () => {
-
   function setCookie(valeur) {
     document.cookie = `city=${valeur}`;
-    console.log(document.cookie);
   }
+
   const btnInput = document.querySelector("#input-submit");
   const input = document.querySelector("#input-text");
-  if (btnInput){
-    btnInput.addEventListener("click", () => {
+
+  if (btnInput) {
+    btnInput.addEventListener("click", (ev) => {
+      ev.preventDefault();
       setCookie(input.value);
-      console.log(document.cookie);
+      window.location = window.location;
     });
   };
 };
